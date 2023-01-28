8b563d11817080241e84a3f24669a1b2af4a4faa
sschacon <sschacon@gmail.com>
2023-01-28 18:38:19 +0000

    BACKUP 28_01_2023

--- srv/sapbackend-exit.cds
+++ srv/sapbackend-exit.cds
@@ -9,3 +9,8 @@ define service SAPBackendExit {
     //entity Incidents as select from external.IncidentsSet;
     entity Incidents as projection on external.IncidentsSet;
 }
+
+@protocol : 'rest'
+service RestService {
+    entity Incidents as projection on SAPBackendExit.Incidents;
+}
--- srv/sapbackend-exit.js
+++ srv/sapbackend-exit.js
@@ -1,46 +1,46 @@
 const cds = require("@sap/cds");
 
-module.exports = cds.service.impl(async function () {
-    const { Incidents } = this.entities;
-    const sapbackend = await cds.connect.to("sapbackend");
-    this.on("READ", Incidents, async (req) => {
-        return await sapbackend.tx(req).send({
-            query: req.query,
-            headers: {
-                Authorization: "Basic c2FwdWk1OnNhcHVpNQ==",
-            }
-        });
-    });
-});
-
-//module.exports = async (srv) => {
+//module.exports = cds.service.impl(async function () {
+//    const { Incidents } = this.entities;
 //    const sapbackend = await cds.connect.to("sapbackend");
-//    const { Incidents } = srv.entities;
-//
-//    srv.on(["READ"], Incidents, async (req) => {
-//        let IncidentsQuery = SELECT.from(req.query.SELECT.from).limit(req.query.SELECT.limit);
-//
-//        if (req.query.SELECT.where) {
-//            IncidentsQuery.where(req.query.SELECT.where);
-//        }
-//
-//        if (req.query.SELECT.orderBy) {
-//            IncidentsQuery.orderBy(req.query.SELECT.orderBy);
-//        }
-//
-//        let incident = await sapbackend.tx(req).send({
-//            query: IncidentsQuery,
+//    this.on("READ", Incidents, async (req) => {
+//        return await sapbackend.tx(req).send({
+//            query: req.query,
 //            headers: {
 //                Authorization: "Basic c2FwdWk1OnNhcHVpNQ==",
-//            },
+//            }
 //        });
-//
-//        let incidents = [];
-//        if (Array.isArray(incident)) {
-//            incidents = incident;
-//        } else {
-//            incidents[0] = incident;
-//        }
-//        return incidents;
 //    });
-//};
+//});
+
+module.exports = async (srv) => {
+    const sapbackend = await cds.connect.to("sapbackend");
+    const { Incidents } = srv.entities;
+
+    srv.on(["READ"], Incidents, async (req) => {
+        let IncidentsQuery = SELECT.from(req.query.SELECT.from).limit(req.query.SELECT.limit);
+
+        if (req.query.SELECT.where) {
+            IncidentsQuery.where(req.query.SELECT.where);
+        }
+
+        if (req.query.SELECT.orderBy) {
+            IncidentsQuery.orderBy(req.query.SELECT.orderBy);
+        }
+
+        let incident = await sapbackend.tx(req).send({
+            query: IncidentsQuery,
+            headers: {
+                Authorization: `${process.env.SAP_GATEWAY_AUTH}`,
+            },
+        });
+
+        let incidents = [];
+        if (Array.isArray(incident)) {
+            incidents = incident;
+        } else {
+            incidents[0] = incident;
+        }
+        return incidents;
+    });
+};
